9923c47a9109c5e2f69273208c816d23364b1140
Bhavya337 <72799302+Bhavya337@users.noreply.github.com>
2021-02-13 10:59:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
-
-function preload() {
-}
-
-function setup() {
-  canvas = createCanvas(300, 300);
-  canvas.center();
+function preload(){
 
 }
 
+function setup(){
+    canvas=createCanvas (350 ,350) ;
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(350, 350);
+    video.hide();
+    
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses );
 
-function draw() {
-  
 }
 
-function take_snapshot(){    
-  save('myFilterImage.png');
+function modelLoaded(){
+    console.log('pose net model is loaded ');
+}
+function  draw(){
+    image(video,0,0,350,350);
+
+}
+function takesnapshot(){
+   save("filterimage.png")
+}
+function gotPoses(results){
+if (results.length>0){
+   console.log(results);
+   console.log("x codinate for nose is "+ results[0].pose.nose.x );
+   console.log("y codinate for nose is "+ results[0].pose.nose.y );
+
+}
 }
