4010a8425f4ac2cb6bc2eeff28b901e1bdc94723
Bhavya337 <72799302+Bhavya337@users.noreply.github.com>
2021-02-15 17:07:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,34 +1,40 @@
-function preload(){
+noseX=0;
+noseY=0;
 
+function preload() {
+  clown_nose = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
 
-function setup(){
-    canvas=createCanvas (350 ,350) ;
-    canvas.center();
-    video = createCapture(VIDEO);
-    video.size(350, 350);
-    video.hide();
-    
-    poseNet=ml5.poseNet(video,modelLoaded);
-    poseNet.on('pose',gotPoses );
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
 
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
 
-function modelLoaded(){
-    console.log('pose net model is loaded ');
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
 }
-function  draw(){
-    image(video,0,0,350,350);
 
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+    noseX = results[0].pose.nose.x-40;
+    noseY = results[0].pose.nose.y;
+  }
 }
-function takesnapshot(){
-   save("filterimage.png")
-}
-function gotPoses(results){
-if (results.length>0){
-   console.log(results);
-   console.log("x codinate for nose is "+ results[0].pose.nose.x );
-   console.log("y codinate for nose is "+ results[0].pose.nose.y );
 
+function draw() {
+  image(video, 0, 0, 300, 300);
+  image(clown_nose, noseX, noseY, 80, 35);
 }
+
+function take_snapshot(){    
+  save('myFilterImage.png');
 }
